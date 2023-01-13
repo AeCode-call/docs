818e236f26d63d8bbb0d3f7e1ff0aac3a2f56435
王端 <wang_duan@haici.com>
2023-01-13 17:19:18 +0800

    feat: test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,17 +4,25 @@ export default defineConfig({
   title: 'xbendan',
   description: 'Just playing around.',
   themeConfig: {
+    editLink: {
+      pattern: 'https://github.com/AeCode-call/docs/blob/master/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
     nav: [
-      { text: 'Guide', link: '/index' },
-      { text: 'Configs', link: '/index' },
-    ]
-  },
-  sidebar: [
-    {
-      text: 'index',
-      items: [
-        { text: 'index', link: '/index' },
+      { text: 'home', link: '/index' },
+      { text: 'myspace', link: '/myspace/index' },
+    ],
+    sidebar: {
+      "/myspace/":[
+        {
+          text: '会议分享',
+          items: [
+            { text: '构建一个 cli 工具集', link: '/myspace/' },
+            { text: '偷懒技巧之批量导出', link: '/myspace/one' },
+          ]
+        }
       ]
     }
-  ]
+  },
+  
 })
---
+++ docs/myspace/one.md
@@ -0,0 +1,7 @@
+
+# 会议分享：偷懒技巧之批量导出
+
+[webpack require.context](https://webpack.docschina.org/guides/dependency-management/#requirecontext)
+
+[vite import.meta.glob](https://cn.vitejs.dev/guide/features.html#glob-import)
+
